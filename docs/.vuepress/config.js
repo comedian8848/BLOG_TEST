6aadc7b9693a1f243526c1b5ef9b331b445165cb
NorthBoat <northboat@163.com>
2022-10-20 18:40:37 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -50,49 +50,49 @@ module.exports = {
               name: '我',
               artist: '张国荣',
               url: 'http://www.ytmp3.cn/down/46480.mp3',
-              cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+              cover: '/logo.jpg'
             },
 	    {
 	      name: 'You Are Beautiful',
               artist: 'James Blunt',
 	      url: 'http://www.ytmp3.cn/down/77296.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '遥远的他',
 	      artist: '陈奕迅',
 	      url: 'http://www.ytmp3.cn/down/64842.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '最冷一天',
 	      artist: '陈奕迅',
 	      url: 'http://www.ytmp3.cn/down/64370.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '倾城',
 	      artist: '陈奕迅',
 	      url: 'http://www.ytmp3.cn/down/64402.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '让一切随风',
 	      artist: '钟镇涛',
 	      url: 'http://www.ytmp3.cn/down/74929.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '唯一',
 	      artist: '告五人',
 	      url: 'http://www.ytmp3.cn/down/75603.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
 	    {
 	      name: '失忆蝴蝶',
 	      artist: '陈奕迅',
 	      url: 'http://www.ytmp3.cn/down/52174.mp3',
-	      cover: 'https://assets.smallsunnyfox.com/music/2.jpg'
+	      cover: '/logo.jpg'
 	    },
           ],
           // 是否默认缩小
